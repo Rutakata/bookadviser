b40c5fb96af293955254af323b8de15b35bce1ff
Rutakata <vladpelishenko14@gmail.com>
2022-07-30 16:47:13 +0300

    Remade header component

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,18 +1,25 @@
-import React from "react";
-import style from "./Header.module.css";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 
-function Header<T extends object>(props: T) {
-    return (
-        <header className={style.header}>
-            <h1 className={style.header__title}>Manga Adviser</h1>
-            <div className={style.actionsWrapper}>
-                <button className={style.header__action}>Random</button>
-                <button className={style.header__action}>Filters</button>
-            </div>
-            <input type="text" className={style.header__searchField}/>
-        </header>
-    )
+function Header() {
+  return (
+    <Box sx={{ flexGrow: 1, marginBottom: "40px", }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Manga Adviser
+          </Typography>
+          <Button color="inherit">Random</Button>
+          <Button color="inherit">Home</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
 
-export default Header
+export default Header;
