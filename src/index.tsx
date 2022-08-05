494a5b263d550cbb8ed52698f5292d7b435d5498
Rutakata <vladpelishenko14@gmail.com>
2022-08-05 20:55:40 +0300

    3

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,15 @@ import './index.css';
 import App from './App';
 import { store } from "./Store/store"
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <App />
         </Provider>
-    </HashRouter>
+    </BrowserRouter>
 );
