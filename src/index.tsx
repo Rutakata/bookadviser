f48b1afdf6db4a949f205e25d0b24c9be2cd04db
Rutakata <vladpelishenko14@gmail.com>
2022-08-26 18:28:53 +0300

    Change to hashrouter for gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,15 @@ import './index.css';
 import App from './App';
 import { store } from "./Store/store"
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <App />
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
 );
